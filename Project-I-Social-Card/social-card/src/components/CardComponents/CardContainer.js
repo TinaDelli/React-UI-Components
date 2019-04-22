ee354ab2ef66766b77de9129047134cb02f835c1
TinaDelli <cecurrier92@gmail.com>
2019-04-22 18:32:25 -0400

    forgot to commit and put together the header and card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+function CardContainer () {
+return (
+<div className="card-container">
+    <a href= "https://www.reactjs.org" target="_blank" rel="noopener noreferrer">
+    <CardBanner />
+    <CardContent />
+    </a>
+</div>
+);
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+function HeaderTitle (){
+    return (
+<div className="header-title">
+<div className="header-header">
+    <h1>Lambda School</h1>
+    <p>@LambdaSchool &bull; 26 Jan</p>
+    </div>
+    <HeaderContent />
+</div>
+
+    );
+}
+
+export default HeaderTitle;
