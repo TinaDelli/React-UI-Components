717634d3d9ad7009915864435963d75e27b4f4e0
TinaDelli <cecurrier92@gmail.com>
2019-04-22 19:27:12 -0400

    added in the numbers to the footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,8 +5,8 @@ function Footer (){
     return (
         <div className="footer-container">
             <i class="far fa-comment"></i>
-            <i class="fas fa-sync"></i>
-            <i class="far fa-heart"></i>
+            <i class="fas fa-sync">  6 </i>
+            <i class="far fa-heart"> 4 </i>
             <i class="far fa-envelope"></i>
         </div>
     );
