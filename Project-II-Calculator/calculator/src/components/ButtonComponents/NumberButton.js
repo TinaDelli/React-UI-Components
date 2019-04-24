80516301bfcfa4b25ef8bb572ee4123b53daf0ac
TinaDelli <cecurrier92@gmail.com>
2019-04-23 23:38:16 -0400

    got calculator up and running

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+
+
+const NumberButton = (props) =>{
+
+return (
+    <button className={props.buttonProp.buttonStyle}> {props.buttonProp.text}</button>
+);
+};
+
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,103 @@
 import React from 'react';
-import './Display.css';
+import './Display.scss';
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButtion from '../ButtonComponents/ActionButton';
+
+const buttons = [
+    {
+        text: "clear",
+        buttonStyle:"wideBox",
+    },
+    {
+        text: "9",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "8",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "7",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "6",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "5",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "4",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "3",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "2",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "1",
+        buttonStyle:"numberStyle",
+    },
+    {
+        text: "0",
+        buttonStyle:"wideBox",
+    }
+    
+];
+
+const action = [
+    
+    {
+        text: "/",
+        buttonStyle:"actionStyle",
+    },
+    {
+        text: "x",
+        buttonStyle:"actionStyle",
+    },
+    {
+        text: "-",
+        buttonStyle:"actionStyle",
+    },
+    {
+        text: "+",
+        buttonStyle:"actionStyle",
+    },
+    {
+        text: "=",
+        buttonStyle:"actionStyle",
+    }
+]
+
+
+function CalculatorDisplay(props){
+    return(
+        <div>
+            <div className="display">
+                <p>0</p>
+            </div>
+            <div className="calcBox" onClick={()=> props.onClick()}>
+            <div className="actionBox">
+            {action.map((x, i) => {
+                return <ActionButtion key={i} prop={x} />
+            })}
+            </div>
+                <div className="numberBox">
+                {props.value}
+                {buttons.map((x,i) => {
+                    return <NumberButton key={i} buttonProp={x} />
+                })}
+                </div>
+            </div>
+        </div>
+   
+    )
+}
+
+export default CalculatorDisplay;
